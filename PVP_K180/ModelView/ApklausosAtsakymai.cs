6375350f2864b07e30b26213f931be43dcc43a6b
Gytis <gytissnei@gmail.com>
2022-04-11 18:59:33 +0300

    Primitive questioner answering system (not finished)

---
+++ PVP_K180/ModelView/ApklausosAtsakymai.cs
@@ -0,0 +1,17 @@
+﻿using PVP_K180.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PVP_K180.ModelView
+{
+    public class ApklausosAtsakymai
+    {
+        public Apklausa apklausa { get; set; }
+        public List<Klausimas> klausimai { get; set; }
+        public List<string> atsakymai { get; set; }
+        public int atsakavo_id { get; set; }
+        public int apklausos_id { get; set; }
+    }
+}
