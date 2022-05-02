c08ad17b8e7262b6f8224c8138ce2f71656062dd
Gediminas <geduxmo@gmail.com>
2022-05-02 23:13:12 +0300

    Prideti renginiu komentarai, trynimas

--- PVP_K180/Controllers/RenginysController.cs
+++ PVP_K180/Controllers/RenginysController.cs
@@ -5,11 +5,13 @@ using System.Web;
 using System.Web.Mvc;
 using PVP_K180.Repos;
 using PVP_K180.Models;
+using PVP_K180.ModelView;
 
 namespace PVP_K180.Controllers
 {
     public class RenginysController : Controller
     {
+        private Renginys_Repos renginys_Repos = new Renginys_Repos();
         // GET: Renginys
         public ActionResult Index()
         {
@@ -79,6 +81,46 @@ namespace PVP_K180.Controllers
                 Response.Write("<script type='text/javascript' language='javascript'> alert('Projektas neredaguotas!')</script>");
             }
             return View(renginys);
+        }
+
+        public ActionResult Komentarai(int id)
+        {
+            KomentaroLangasRenginys komentaruLangas = new KomentaroLangasRenginys();
+            komentaruLangas.parasytiKomentarai = renginys_Repos.Gauti_Renginio_Komentarus(id);
+            TempData["RenginioID"] = id;
+            return View(komentaruLangas);
+        }
+
+        [HttpPost]
+        public ActionResult Komentarai(int id, KomentaroLangasRenginys komentaras)
+        {
+            komentaras.rasomasKomentaras.fk_Vartotojasid_Vartotojas = Convert.ToInt32(Session["UserID"]);
+            komentaras.rasomasKomentaras.parasymo_data = DateTime.Now;
+            komentaras.rasomasKomentaras.priskirtas_id = id;
+            renginys_Repos.Rasyti_Komentara(komentaras.rasomasKomentaras);
+            TempData["SuccsessComment"] = "Komentaras sėkmingai parašytas";
+            return RedirectToAction("Komentarai", new { id = id });
+        }
+
+        public ActionResult TrintiKomentara(int id)
+        {
+            if (Session["UserID"] == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            bool check = renginys_Repos.PatikrintiArGalimaTrinti(Convert.ToInt32(Session["UserID"]), id);
+            if (check)
+            {
+                renginys_Repos.Trinti_Komentara(id);
+                TempData["DeleteSucc"] = "Komentaras ištrintas sėkmingai";
+            }
+            else
+            {
+                TempData["DeleteFail"] = "Komentaro nepavyko ištrinti";
+            }
+
+            return RedirectToAction("Komentarai", new { id = Convert.ToInt32(TempData["RenginioID"]) });
         }
     }
 }
