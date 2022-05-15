528475b5e19d6de123916077f6245fc010681c1e
Gytis <gytissnei@gmail.com>
2022-05-15 12:13:42 +0300

    Incident edit state form
    Now admin can review incident information and edit incident state and if admin has some comments it can post them.

---
+++ PVP_K180/ModelView/AtsitikimuKeitimasPerziura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PVP_K180.ModelView
+{
+    public class AtsitikimuKeitimasPerziura
+    {
+        [DisplayName("Atsitikimo id")]
+        public int id_Atstikimas { get; set; }
+
+        [DisplayName("Komentaras")]
+        public string komentaras { get; set; }
+
+        [DisplayName("Atsitikimo būsena")]
+        public int atsitikimo_busena { get; set; }
+
+        public int tvirtintojas { get; set; }
+
+        public IList<SelectListItem> atsitikimo_busenos { get; set; }
+    }
+}
