8342c63c7835ced5fca1398ebdbe511039c7a9f2
Gediminas <geduxmo@gmail.com>
2022-05-18 01:54:08 +0300

    Keisti Renginiu busenas

---
+++ PVP_K180/ModelView/RenginioBusenaPerziura.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PVP_K180.ModelView
+{
+    public class RenginioBusenaPerziura
+    {
+        [DisplayName("Renginio būsena")]
+        public IEnumerable<SelectListItem> Busenos { get; set; }
+
+        public int renginio_Busena { get; set; }
+    }
+}
